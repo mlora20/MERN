d7ce1c407f0f1d5b20954cc2e307e7ed68bd1aa6
mlora <marcpuigcerda@gmail.com>
2023-03-28 23:11:38 +0200

    afegir routes i react route

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import ListaUsuarios from './ListaUsuarios';
 import AgregarUsuarios from './AgregarUsuarios';
 import EditarUsuarios from './EditarUsuarios';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>CRUD MERN</h1>
-      <ListaUsuarios/>
-      <AgregarUsuarios/>
-      <EditarUsuarios/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ListaUsuarios/>} exact></Route>
+          <Route path='/AgregarUsuarios' element={<AgregarUsuarios/>} exact></Route>
+          <Route path='/EditarUsuarios' element={<EditarUsuarios/>} exact></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
