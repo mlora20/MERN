489dab943268eae25e761c636c014d11d38e94ec
mlora <marcpuigcerda@gmail.com>
2023-03-29 22:34:47 +0200

    paquetes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require ('express');
 const app = express();
-
+app.get('/',(req,res)=>{
+    res.end('Hola server')
+})
 
 //configurar server basico
 app.listen(5000,function(){
-    console.log('elservidor esta corrents')
+    console.log('el servidor esta corrents')
 })
