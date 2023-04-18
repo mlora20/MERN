13355dbc1a55e618caf432a4188aeacad3db42cb
mlora <marcpuigcerda@gmail.com>
2023-04-18 23:30:32 +0200

    bootstrap

--- cliente/src/AgregarUsuarios.js
+++ cliente/src/AgregarUsuarios.js
@@ -1,11 +1,26 @@
 ///imporem la llibreria react del json
 import React from 'react';
 
+function AgregarUsuario(){
+
+}
+
 function AgregarUsuarios(){
     return(
-        <div>
+        <div className='container'>
+            <div className='row'>
             <h2>Agreagar usuarios</h2>
-        </div>
+                <div className='col-sm-6 offset-3'>
+                    <label htmlFor="nombre" className="form-label">Nombre</label>
+                    <input type="text" className="form-control"></input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control"></input>
+                </div>
+                    <button onClick={AgregarUsuario} className="btn btn-success">Guardar Usuario</button>
+                </div>
+            </div>
     )
 }
 
--- cliente/src/App.js
+++ cliente/src/App.js
@@ -8,7 +8,24 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1>CRUD MERN</h1>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="AgregarUsuario">AgregarUsuario</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<ListaUsuarios/>} exact></Route>
